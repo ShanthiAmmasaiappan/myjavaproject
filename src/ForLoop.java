243ec5d75a42d1eccfc02e3c7bbc36c137ab3045
ShanthiAmmasaiappan <mailtoshanthi@gmail.com>
2022-10-02 13:57:52 -0400

    test

---
+++ src/ForLoop.java
@@ -0,0 +1,11 @@
+
+public class ForLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for( int count=0; count<=3;count++) {
+			System.out.println("print");
+		}
+	}
+
+}
