273e191f96855c2cacbd06057c77cf2090b2a2ba
ShanthiAmmasaiappan <mailtoshanthi@gmail.com>
2022-11-03 10:53:25 -0400

    updated

--- src/cruisebooking/UserDetailClass.java
+++ src/cruisebooking/UserDetailClass.java
@@ -24,31 +24,90 @@ public class UserDetailClass {
 
 	Scanner sc = new Scanner(System.in);
 
-	void userRegistrationDetails() {
-		System.out.println("Enter your email address:");
-		emailId = sc.nextLine();
-		System.out.println("Enter the password that you want to use.");
-		passWord = sc.nextLine();
-		System.out.println("Enter your Full Name.");
-		fullName = sc.nextLine();
-		System.out.println("Enter your phone number.");
-		phoNumber = sc.nextLine();
-	}
-
 	public void setEmailId(String emailId) {
 		this.emailId = emailId;
+	}
+
+	boolean validateEmailId(String emailId) {
+		boolean isEmailValid;
+		do {
+			System.out.println("Enter your email address:");
+			emailId = sc.nextLine();
+
+			if (emailId.matches("^[a-zA-Z0-9_.]+@[a-zA-Z.]+?\\.[a-zA-Z]{2,3}$")) {
+
+				isEmailValid = true;
+			} else {
+				System.out.println("Please enter an valid email with '@' and'.'");
+				isEmailValid = false;
+			}
+		} while (!emailId.matches("^[a-zA-Z0-9_.]+@[a-zA-Z.]+?\\.[a-zA-Z]{2,3}$"));
+		return isEmailValid;
 	}
 
 	public void setPassWord(String password) {
 		this.passWord = password;
 	}
 
+	boolean validatePassword(String password) {
+		boolean isPasswordValid;
+		do {
+			System.out.println("Enter the password that you want to use.");
+			passWord = sc.nextLine();
+
+			if (passWord
+					.matches("^(?=.*[0-9])" + "(?=.*[a-z])(?=.*[A-Z])" + "(?=.*[@#$%^&+=])" + "((?=\\S+$).{8,20})$")) {
+				isPasswordValid = true;
+			} else {
+				System.out.println("Doesnot contain the matches (a-z),(A-Z),(0-9) and 1 special character");
+				isPasswordValid = false;
+
+			}
+		} while (!passWord.matches("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])((?=\\S+$).{8,20})$"));
+		return isPasswordValid;
+	}
+
 	public void setFullname(String fulName) {
 		this.fullName = fulName;
 	}
 
+	boolean validateFullName(String fulName) {
+		boolean isFullNameValid;
+		do {
+			System.out.println("Enter your Full Name.");
+			fullName = sc.nextLine();
+
+			if (fullName.matches("^((?=.*[a-z])(?=.*[A-Z]).{8,20})$")) {
+				isFullNameValid = true;
+			} else {
+				System.out.println("Doesnot contain the matches (a-z),(A-Z) and character between 8 - 20.");
+				isFullNameValid = false;
+
+			}
+		} while (!fullName.matches("^((?=.*[a-z])(?=.*[A-Z]).{8,20})$"));
+		return isFullNameValid;
+	}
+
 	public void setPhoneNumber(String phoneNumber) {
 		this.phoNumber = phoneNumber;
+	}
+
+	boolean validatePhoneNumber(String phoneNumber) {
+		boolean isPhoneNumberValid;
+		do {
+			System.out.println("Enter your phone number.");
+			phoNumber = sc.nextLine();
+
+			if (phoNumber.matches("^(\\+\\d{1,3}[- ]?)?\\d{10}$")) {
+				isPhoneNumberValid = true;
+			} else {
+				System.out.println("Doesnot contain the matches of length 10.");
+				isPhoneNumberValid = false;
+
+			}
+		} while (!phoNumber.matches("^(\\+\\d{1,3}[- ]?)?\\d{10}$"));
+		return isPhoneNumberValid;
+
 	}
 
 	void updateLoginDetails() {
@@ -57,31 +116,35 @@ public class UserDetailClass {
 			username = sc.nextLine();
 			System.out.println("Enter the password .");
 			password = sc.nextLine();
+			do {
+				if ((this.emailId.equalsIgnoreCase(username) || this.fullName.equals(username))
+						&& this.passWord.equals(password)) {
+					System.out.println("Login is successful.\n");
 
-			if ((this.emailId.equalsIgnoreCase(username) || this.fullName.equals(username))
-					&& this.passWord.equals(password)) {
-				System.out.println("Login is successful.\n");
-
-			} else {
-				count--;
-				System.out.println("Username and Password doesnot match.");
-
-			}
+				} else {
+					count--;
+					System.out.println("Username and Password doesnot match.");
+					break;
+				}
+			} while (!(this.emailId.equalsIgnoreCase(username) || this.fullName.equals(username)));
 
 			if (count < 1) {
 				System.out.println("Verify your login details.");
 				System.exit(0);
 			}
-		} while (!this.emailId.equalsIgnoreCase(username) && !this.passWord.equals(password));
+		} while (!(this.emailId.equalsIgnoreCase(username) || this.fullName.equals(username))
+				&& this.passWord.equals(password));
 		System.out.println("Press 'Y' to continue");
 		String enter = sc.nextLine();
 		if (enter.equalsIgnoreCase("Y")) {
 		} else {
 			System.exit(0);
 		}
+
 	}
 
 	public void validatePassword(String existingPassword, String newPassword) {
+
 		do {
 			System.out.println("Enter your existing password ");
 			existingPassword = sc.next();
@@ -90,6 +153,9 @@ public class UserDetailClass {
 			numberOfAttempt++;
 			if (existingPassword.equals(newPassword)) {
 				System.out.println("Entered password already exist.Please try again");
+			} else if (!newPassword
+					.matches("^(?=.*[0-9])" + "(?=.*[a-z])(?=.*[A-Z])" + "(?=.*[@#$%^&+=])" + "((?=\\S+$).{8,20})$")) {
+				System.out.println("Doesnot contain the matches (a-z),(A-Z),(0-9) and 1 special character");
 			} else if (this.passWord.equals(existingPassword)) {
 				this.passWord = newPassword;
 				System.out.println("Your password has been changed!");
@@ -118,6 +184,8 @@ public class UserDetailClass {
 			numberOfAttempt++;
 			if (existingPhoneNumber.equals(newPhoneNumber)) {
 				System.out.println("Entered phonenumber already exist.Please try again.");
+			} else if (!newPhoneNumber.matches("^(\\+\\d{1,3}[- ]?)?\\d{10}$")) {
+				System.out.println("Doesnot contain the matches of length 10. Please try again.");
 			} else if (this.phoNumber.equals(existingPhoneNumber)) {
 				this.phoNumber = newPhoneNumber;
 				System.out.println("Your phonenumber has been changed!");
@@ -146,6 +214,8 @@ public class UserDetailClass {
 			numberOfAttempt++;
 			if (existingEmail.equals(newEmail)) {
 				System.out.println("Entered Email already exist.Please try again");
+			} else if (!newEmail.matches("^[a-zA-Z0-9_.]+@[a-zA-Z.]+?\\.[a-zA-Z]{2,3}$")) {
+				System.out.println("Please enter an valid email with '@' and'.'. Please try again");
 			}
 
 			else if (this.emailId.equals(existingEmail)) {
