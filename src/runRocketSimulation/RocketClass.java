63e5d501672bd347edf889cc21768acb73e0db8e
ShanthiAmmasaiappan <mailtoshanthi@gmail.com>
2022-11-17 18:16:15 -0500

    Final_Project

--- src/runRocketSimulation/RocketClass.java
+++ src/runRocketSimulation/RocketClass.java
@@ -4,9 +4,9 @@ import java.util.ArrayList;
 
 public class RocketClass implements SpaceShip {
 
-	int cost;
-	int currentWeight;
-	int maxWeight;
+	protected double cost;
+	protected int currentWeight;
+	protected int maxWeight;
 
 	@Override
 	public boolean launch() {
@@ -31,14 +31,13 @@ public class RocketClass implements SpaceShip {
 	}
 
 	@Override
-//	 Function to modify the arrayList
+	// method to load items in a rocket and return the remaining items
 	public ArrayList<Item> loadItems(ArrayList<Item> itemsToLoad) {
 
 		ArrayList<Item> itemsNotLoaded = new ArrayList<Item>();
 
 		// Sequential Loading
-//		for-each" loop, which is used exclusively to loop through elements in an array
-//		for (type variableName : arrayName)
+
 		for (Item curr_item : itemsToLoad) {
 			if (canCarry(curr_item)) {
 				carry(curr_item);
@@ -50,8 +49,12 @@ public class RocketClass implements SpaceShip {
 		return itemsNotLoaded;
 	}
 
-	public int getCost() {
+	public double getCost() {
 		return cost;
+	}
+
+	public int getCurrentWeight() {
+		return currentWeight;
 	}
 
 }
